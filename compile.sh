77221955434f036a55ac6000d4d0ef75a83fd847
Léo Tomas <ltomas@enseirb-matmeca.fr>
2021-03-17 21:47:53 +0100

    commit

--- compile.sh
+++ compile.sh
@@ -1,1 +1,15 @@
-cd src/java && gradle build && cd ../cpp && make all;
+
+
+if [ "$#" -ne 1 ]; then
+	echo "The number of inputted action(s) should be 1."
+else
+	if [ $1 = "compile"  ]; then
+		cd src/java && gradle build && cd ../cpp && make all;
+	else
+		if [ $1 = "clean" ]; then
+			cd src/java && gradle clean;
+		else
+			echo "Action not recognised. (compile/clean)";
+		fi
+	fi
+fi
